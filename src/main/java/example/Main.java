10254a0ec353c681b72611ba01b0908c0d450a99
João <joao.cardoso@aluno.facthus.com.br>
2021-04-12 20:57:20 -0300

    Alteração de Teste :V

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Eclipse Git Test");
+		System.out.println("Será que foi ?");
+		System.out.println("nao sei");
 	}
 
 }
